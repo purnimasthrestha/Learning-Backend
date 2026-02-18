90d1a805a8aa9da7ea9a24816941c1713165cb73
purnimashrestha <sthapurnima90@gmail.com>
2026-02-18 14:17:10 +0545

    project setup for backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express();
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+});
+
+app.get("/", (req, res) => {
+    res.send("Purnima .. Backend is running")
+
+});
+
